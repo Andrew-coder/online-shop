08294499bcc2ca1cad91a36815ed2ce358e6f315
Andriy Ivanyuk <Andriy Ivanyuk>
2017-01-18 11:09:37 +0200

    Utils package was added with utility classes for extracting result set

---
+++ src/main/java/online/shop/dao/utils/ResultSetExtractor.java
@@ -0,0 +1,7 @@
+package online.shop.dao.utils;
+
+/**
+ * Created by andri on 1/17/2017.
+ */
+public interface ResultSetExtractor {
+}
---
+++ src/main/java/online/shop/dao/utils/impl/UserResultSetExtractor.java
@@ -0,0 +1,7 @@
+package online.shop.dao.utils.impl;
+
+/**
+ * Created by andri on 1/17/2017.
+ */
+public class UserResultSetExtractor {
+}
