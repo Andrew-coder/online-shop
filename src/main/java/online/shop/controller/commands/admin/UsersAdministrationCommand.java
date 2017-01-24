61860b3ee26d60c4d8eecc1f81ddb4d58095cc03
Andriy Ivanyuk <Andriy Ivanyuk>
2017-01-24 13:49:14 +0200

    commands add/remove user from blacklist were implemented

---
+++ src/main/java/online/shop/controller/commands/admin/UsersAdministrationCommand.java
@@ -0,0 +1,7 @@
+package online.shop.controller.commands.admin;
+
+/**
+ * Created by andri on 1/23/2017.
+ */
+public class UsersAdministrationCommand {
+}
---
+++ src/main/java/online/shop/controller/commands/admin/blacklist/RemoveBlacklistCommand.java
@@ -0,0 +1,7 @@
+package online.shop.controller.commands.admin.blacklist;
+
+/**
+ * Created by andri on 1/24/2017.
+ */
+public class RemoveBlacklistCommand {
+}
